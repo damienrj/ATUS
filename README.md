34bbdedd3f15c4035d0e1749de34c9de1db9cb5a
damienrj <damienrj@Jennifers-MacBook-Pro.local>
2015-01-08 19:56:14 -0800

    Made function to download data
    Exclude the data folder from git

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 American Time Use Survey Project
 
 This project is to explore the data provided from the [American Time Use Survey](http://www.bls.gov/tus/)
+
+The main pdfs have been added to the folder. The link describing how to use the data is located [here](http://www.bls.gov/tus/howto.htm)
+
+To download the raw files run the following script: download_data
---
+++ download_data.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 19:26:40 2015
+
+@author: damienrj
+"""
+
+import urllib
+
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atusresp_0313.zip", "./data/atusresp_0313.zip")
+print("Download 1 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atusrost_0313.zip", "./data/atusrost_0313.zip")
+print("Download 2 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atusact_0313.zip", "./data/atusact_0313.zip")
+print("Download 3 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atussum_0313.zip", "./data/atussum_0313.zip")
+print("Download 4 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atuswho_0313.zip", "./data/atuswho_0313.zip")
+print("Download 5 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atuscps_0313.zip", "./data/atuscps_0313.zip")
+print("Download 6 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atusrostec_1113.zip", "./data/atusrostec_1113.zip")
+print("Download 7 done")
+urllib.urlretrieve ("http://www.bls.gov/tus/special.requests/atuswgts_0313.zip", "./data/atuswgts_0313.zip")
+print("Download 8 done")
