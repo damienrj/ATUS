8d6ccf5b96ee44b9974f1d5edc68b2db46266dad
damienrj <damienrj@Jennifers-MacBook-Pro.local>
2015-02-06 20:13:40 -0800

    LA vs NY update

--- Sleeping Data/sleep_plots_drj.py
+++ Sleeping Data/sleep_plots_drj.py
@@ -37,10 +37,28 @@ data = results[2013].unstack(1)
 data.plot(ax=f.gca(), kind='bar', ylim=(7,10), y = ['Male', 'Female'])
 print(data)
 
+#This section finds the number of people in eac state, each year. 
 grouped_state = df.groupby(['YEAR', 'STATEFIP'])
 num_per_state = grouped_state.size()
 num_per_state = num_per_state.unstack(0)
 
 d = {'Mean' : num_per_state.mean(1), 'STD' : num_per_state.std(1)}
 pd.DataFrame(d)
+
+
+#This section groups the data by metro area
+grouped = df.groupby(['YEAR', 'METAREA', 'SEX' ] )
+num_LA_NY=grouped.size().unstack(0).loc[[4481, 5601]]
+results = grouped.weighted_sleep.sum() / grouped.WT06.sum()
+results = results.unstack(0)/60
+la_ny_sleep = results.loc[[4481, 5601]]
+la_ny_sleep = la_ny_sleep.rename(index={4481:"LA Metro", 5601: "NY Metro"})
+la_ny_sleep = la_ny_sleep.stack(0).unstack(1)
+
+
+
+f = plt.figure()
+f.gca().get_xaxis().get_major_formatter().set_useOffset(False)
+la_ny_sleep['Male'].unstack(0).plot(ax = f.gca())
+plt.ylabel("Hours Slept")
 
